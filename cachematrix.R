61dc2d20bc5ef80206855c6b8eb68d62cede4dbd
alanxuefei <alan.xuefei@googlemail.com>
2015-08-11 11:32:20 +0800

    CachedMatrix is done
    CachedMatrix is done

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
+
 makeCacheMatrix <- function(x = matrix()) {
-
+    InverseMatrix <- NULL
+    list(
+      get = function() x,
+      setInverseMatrix = function(InverseMatrix0) InverseMatrix <<- InverseMatrix0,
+      getInverseMatrix = function() InverseMatrix
+    )
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getInverseMatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  data
+  InverseMatrix <- solve(data)
+  x$setInverseMatrix(InverseMatrix)
+  InverseMatrix
 }
+
+N=99
+NormalMatrix<-matrix(rnorm(N*N,mean=0,sd=1), N, N)
+CachedMatrix=makeCacheMatrix(NormalMatrix)
+system.time(cacheSolve(CachedMatrix))
+system.time(cacheSolve(CachedMatrix))
