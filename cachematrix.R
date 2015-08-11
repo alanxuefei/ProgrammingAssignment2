bdcc6c6e79f6cc61275ebfcfc5a0911ae7b06c42
alanxuefei <alan.xuefei@googlemail.com>
2015-08-11 11:04:01 +0800

    Revert "Some progress on makeCacheMatrix"
    This reverts commit d89ca2f5563ca3825fdb6c8bad44a6937ae3f3e3.

--- cachematrix.R
+++ cachematrix.R
@@ -3,24 +3,13 @@
 
 ## Write a short comment describing this function
 
-
 makeCacheMatrix <- function(x = matrix()) {
-  
-  makeVector2 <- function(x = numeric()) {
-    InverseMatrix <<- NULL
-    list(
-      get = function() x
-      setInverseMatrix <- function(InverseMatrix0) InverseMatrix <<- InverseMatrix0
-      getInverseMatrix <- function() InverseMatrix
-    )
-  }
+
 }
+
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
-
-N=10
-matrix( rnorm(N*N,mean=0,sd=1), N, N) 
